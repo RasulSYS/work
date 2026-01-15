b63896a2902c5b3318c8a7676106e58afcd8c69f
Rasul <mrrasulchik@mail.ru>
2026-01-15 19:47:42 +0500

    zadacha 3

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-const numberOfFilms = +prompt("qancha film ko'rgansiz", '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+function firstTask() {
+    for (let i = 5; i <= 10; i++) {
+        console.log(i);
+    }
 }
-const   a = prompt("oxirgi ko'rgan filmizgiz nomi?", ''),
-        b = +prompt("filmni baholang", ''),
-        c = prompt("oxirgi ko'rgan filmizgiz nomi?", ''),
-        d = +prompt("filmni baholang", '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+firstTask();
+
+function secondTask() {
+    for(let i = 20; i >= 10; i--){
+    if(i === 13){
+        break;
+    }
+    console.log(i);
+    }
+}
+secondTask();
+
+function thirdTask(){
+    for(let i = 2; i <= 10; i++){
+        if(i % 2 === 0){
+            console.log(i);
+        }
+    }
+}
+thirdTask();
+
+function fourthTask(){ 
+    let i = 2;
+    while(i < 16){
+        i++;
+        if (i % 2 === 0){
+            continue;        
+        }else{
+            console.log(i);        
+        }
+    }
+}
+fourthTask();
+
+function fifthTask(){
+    const arr = [];
+    for(let i = 5; i <= 10; i++){
+    arr[i-5]=i;   
+    }
+    console.log(arr);
+    return arr;
+}
+fifthTask();
